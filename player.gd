a30d695f820ceb27a167d7c05ec424f9397aca77
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-10-04 04:38:26 +0200

    input system update

--- player.gd
+++ player.gd
@@ -2,20 +2,19 @@ extends CharacterBody2D
 
 var gravity = 9
 var walk_speed = 50
-var jump_force = -100
+var jump_force = 100
 
 func _ready():
 	print("Game Started")
 
 func _physics_process(delta):
 	velocity.y += gravity
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_key_pressed(KEY_A):
 		velocity.x = -walk_speed
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_key_pressed(KEY_D):
 		velocity.x = walk_speed
 	else:
 		velocity.x = 0
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_key_pressed(KEY_SPACE):
 		velocity.y = -jump_force
 	move_and_slide()
-
